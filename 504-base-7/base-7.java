6fdc054e173b5b8c81b3d42c76f55c6828123146
Thanigai-S <sthanigai07@gmail.com>
2026-01-20 12:48:55 +0530

    Time: 1 ms (73.22%) | Memory: 42.8 MB (51.30%) - LeetSync

---
+++ 504-base-7/base-7.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String convertToBase7(int num)
+    {
+        if(num==0) return "0";
+        boolean neg = num < 0;
+        if(num<0) num*=-1;
+        StringBuilder sb = new StringBuilder();
+        while(num>0)
+        {
+            sb.append(num%7);
+            num/=7;
+        }
+        if(neg) sb.append("-");
+        return sb.reverse().toString();
+    }
+}
