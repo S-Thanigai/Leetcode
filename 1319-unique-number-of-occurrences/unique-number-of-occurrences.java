8052df3f31615c3b9c4215c0f91ed228a352ebdb
Thanigai-S <sthanigai07@gmail.com>
2026-03-03 00:05:18 +0530

    Time: 2 ms (98.57%) | Memory: 43.6 MB (84.58%) - LeetSync

---
+++ 1319-unique-number-of-occurrences/unique-number-of-occurrences.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean uniqueOccurrences(int[] arr)
+    {
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int i:arr)
+        {
+            map.put(i,map.getOrDefault(i,0)+1);
+        }
+        HashSet<Integer> s = new HashSet<>(map.values());
+        return s.size()==map.size();
+    }
+}
