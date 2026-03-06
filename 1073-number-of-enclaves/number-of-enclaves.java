505010999e2cac1a93fc19496f03dda465c5ab39
Thanigai-S <sthanigai07@gmail.com>
2026-03-06 22:33:13 +0530

    Time: 6 ms (98.96%) | Memory: 60.9 MB (41.42%) - LeetSync

---
+++ 1073-number-of-enclaves/number-of-enclaves.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int numEnclaves(int[][] grid)
+    {
+        int c = 0;
+        int m = grid.length;
+        int n = grid[0].length;
+        for(int j=0;j<n;j++) dfs(grid,0,j);
+        for(int j=0;j<n;j++) dfs(grid,m-1,j);
+        for(int i=0;i<m;i++) dfs(grid,i,0);
+        for(int i=0;i<m;i++) dfs(grid,i,n-1);
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(grid[i][j]==1)
+                {
+                    c++;
+                }
+            }
+        }
+        return c;
+    }
+    public void dfs(int grid[][],int r,int c)
+    {
+        if(r<0 || c<0 || r>=grid.length || c>=grid[0].length || grid[r][c]!=1)
+        {
+            return;
+        }
+        grid[r][c]=0;
+        dfs(grid,r+1,c);
+        dfs(grid,r-1,c);
+        dfs(grid,r,c+1);
+        dfs(grid,r,c-1);
+    }
+}
