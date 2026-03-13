728e7a8c2716abc32e5fde50d4fa797a874d9761
Thanigai-S <sthanigai07@gmail.com>
2026-03-13 10:31:19 +0530

    Time: 11 ms (95.58%) | Memory: 47.1 MB (99.21%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -6,13 +6,40 @@ class Solution {
         {
             map.put(nums[i],map.getOrDefault(nums[i],0)+1);
         }
-        List<Integer> li = new ArrayList<>(map.keySet());
-        li.sort((a,b) -> map.get(b) - map.get(a));
-        int arr[] = new int[k];
-        for(int i=0;i<k;i++)
+        // List<Integer> li = new ArrayList<>(map.keySet());
+        // li.sort((a,b) -> map.get(b) - map.get(a));
+        // int arr[] = new int[k];
+        // for(int i=0;i<k;i++)
+        // {
+        //     arr[i] = li.get(i);
+        // } 
+        // return arr;
+        List<Integer>[] li = new ArrayList[nums.length+1];
+        for(int ke:map.keySet())
         {
-            arr[i] = li.get(i);
-        } 
-        return arr;
+            int f = map.get(ke);
+            if(li[f]==null)
+            {
+                li[f] = new ArrayList<>();
+            }
+            li[f].add(ke);
+        }
+        int res[] = new int[k];
+        int id = 0;
+        for(int i=li.length-1;i>=0 && id<k;i--)
+        {
+            if(li[i]!=null)
+            {
+                for(int a:li[i])
+                {
+                    res[id++] = a;
+                    if(id==k)
+                    {
+                        break;
+                    }
+                }
+            }
+        }
+        return res;
     }
 }
