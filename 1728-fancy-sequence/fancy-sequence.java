10fe18e65e66931aa38718e22e6ab4a8da09be65
Thanigai-S <sthanigai07@gmail.com>
2026-03-15 16:47:15 +0530

    Time: 45 ms (93.10%) | Memory: 124.6 MB (50.00%) - LeetSync

---
+++ 1728-fancy-sequence/fancy-sequence.java
@@ -0,0 +1,63 @@
+class Fancy {
+    static final long MOD = 100_00_00_007;
+    List<Long> arr;
+    long add,mul;
+    public Fancy() {
+        arr = new ArrayList<>();
+        mul=1;
+        add=0;
+    }
+    long modPow(long a, long b) {
+        long res = 1;
+        a %= MOD;
+
+        while (b > 0) {
+            if ((b & 1) == 1)
+                res = (res * a) % MOD;
+
+            a = (a * a) % MOD;
+            b >>= 1;
+        }
+
+        return res;
+    }
+
+    long modInverse(long x) {
+        return modPow(x, MOD - 2);
+    }
+
+    public void append(int val)
+    {
+        long st = ((val - add + MOD) % MOD * modInverse(mul) % MOD);
+        arr.add(st);
+    }
+    
+    public void addAll(int inc)
+    {
+        add = (add+inc) % MOD;
+    }
+    
+    public void multAll(int m)
+    {
+        mul = (mul*m) % MOD;
+        add = (add*m) % MOD;
+    }
+    
+    public int getIndex(int idx)
+    {
+        if(idx >= arr.size())
+        {
+            return -1;
+        }
+        return (int)((arr.get(idx) * mul + add) % MOD);
+    }
+}
+
+/**
+ * Your Fancy object will be instantiated and called as such:
+ * Fancy obj = new Fancy();
+ * obj.append(val);
+ * obj.addAll(inc);
+ * obj.multAll(m);
+ * int param_4 = obj.getIndex(idx);
+ */
