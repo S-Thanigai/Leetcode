2ca3e4202e33f0ca0c2c6e1f6d0a3c5500e00073
Thanigai-S <sthanigai07@gmail.com>
2026-03-19 17:09:01 +0530

    Time: 19 ms (13.37%) | Memory: 47.8 MB (47.91%) - LeetSync

--- 645-set-mismatch/set-mismatch.java
+++ 645-set-mismatch/set-mismatch.java
@@ -1,7 +1,7 @@
 class Solution {
     public int[] findErrorNums(int[] nums)
     {
-        int n = nums.length;
+        /*int n = nums.length;
         int arr[] = new int[n+1];
         for(int nu:nums)
         {
@@ -16,6 +16,29 @@ class Solution {
                 du=i;
             }
             else if(arr[i]==0)
+            {
+                mi=i;
+            }
+        }
+        return new int[]{du,mi};*/
+        int n = nums.length;
+        Map<Integer,Integer> hm = new HashMap<>();
+        for(int nu:nums)
+        {
+            hm.put(nu,hm.getOrDefault(nu,0)+1);
+        }
+        int du = -1;
+        int mi = -1;
+        for(int i=1;i<=n;i++)
+        {
+            if(hm.containsKey(i))
+            {
+                if(hm.get(i)==2)
+                {
+                    du=i;
+                }
+            }
+            else
             {
                 mi=i;
             }
