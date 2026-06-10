e8067db9356ada85b54f99d4aa68ecd1ab2ad1ad
Thanigai-S <sthanigai07@gmail.com>
2026-06-10 19:54:24 +0530

    Time: 1 ms (51.19%) | Memory: 44.7 MB (77.38%) - LeetSync

---
+++ 2341-count-prefixes-of-a-given-string/count-prefixes-of-a-given-string.java
@@ -0,0 +1,15 @@
+class Solution
+{
+    public int countPrefixes(String[] words, String s)
+    {
+        int c = 0;
+        for(String w:words)
+        {
+            if(s.startsWith(w))
+            {
+                c+=1;
+            }
+        }
+        return c;
+    }
+}
