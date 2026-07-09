fa8af58208aa377aa1933a3ad22dc32b97ea20be
Thanigai-S <sthanigai07@gmail.com>
2026-07-09 13:37:14 +0530

    Time: 1491 ms (9.87%) | Memory: 0B (100.00%) - LeetSync

--- 185-department-top-three-salaries/department-top-three-salaries.sql
+++ 185-department-top-three-salaries/department-top-three-salaries.sql
@@ -1,13 +1,10 @@
-WITH new_table AS (
-    SELECT 
-        d.name AS Department,
-        e.name AS Employee,
-        e.salary AS Salary,
-        DENSE_RANK() OVER(PARTITION BY d.name ORDER BY e.salary DESC) AS ranking
-    FROM Employee e
-    LEFT JOIN Department d
-    ON e.departmentId = d.id
+with tab as
+(
+    select d.name as Department,
+    e.name as Employee,
+    e.salary as Salary,
+    dense_rank() over(partition by e.departmentId order by e.salary desc) as rk
+    from Employee e join Department d on e.departmentId = d.id
 )
-SELECT Department, Employee, Salary
-FROM new_table
-WHERE ranking <= 3;
+select Department,Employee,Salary from tab
+where rk<=3;
